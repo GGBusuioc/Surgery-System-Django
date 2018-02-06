163d9ad639bbad68f6e5ea4f64af4f51680cfec1
George-Gabriel <sc15ggb@leeds.ac.uk>
2018-02-06 15:11:31 +0000

    Chapter 2 - Second Part

--- surgerysystem/surgerysystem/urls.py
+++ surgerysystem/surgerysystem/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from surgerysystem.views import hello
+from django.urls import include, path, re_path
+from surgerysystem.views import hello, current_datetime, hours_ahead
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello)
+    path('hello/', hello),
+    path('time/', current_datetime),
+    path('another-time-page/', current_datetime),
+    re_path('time/plus/(\d{1,2})/$', hours_ahead),
 ]
