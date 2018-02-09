1fb2e21b3f7129d1cdbea3236a79a130b9163943
George-Gabriel <sc15ggb@leeds.ac.uk>
2018-02-09 16:04:00 +0000

    Full ogin functionality incoming

--- surgerysystem/surgerysystem/views.py
+++ surgerysystem/surgerysystem/views.py
@@ -8,13 +8,14 @@ rooms = [1,2,3,4,5,6,7,8,9,10]
 doctors_avail = []
 rooms_busy = []
 
+
 def position(request, patient_id):
     try:
         patient_id = int(patient_id)
     except ValueError:
         raise Http404()
-
-    return HttpResponse("Your(id %d) are number %d in the queue" % (patient_id, patient_queue.index(patient_id)+1))
+    if request.method == "GET":
+        return HttpResponse("Your(id %d) are number %d in the queue" % (patient_id, patient_queue.index(patient_id)+1))
 
 def checkin(request, patient_id):
     try:
@@ -22,16 +23,16 @@ def checkin(request, patient_id):
     except ValueError:
         raise Http404()
 
-    patient_queue.append(patient_id)
+    if request.method == "POST":
+        patient_queue.append(patient_id)
 
-    if doctors_avail:
-        doctors_avail.pop(0)
-        room_number = rooms[0]
-        rooms.pop(0)
-        return HttpResponse("Please proceed to room %d" %  (room_number))
-    else:
-        return HttpResponse("All doctors are busy right now, please relax in the waiting area. You are number %d in the queue." % (patient_queue.index(patient_id)+1))
-
+        if doctors_avail:
+            doctors_avail.pop(0)
+            room_number = rooms[0]
+            rooms.pop(0)
+            return HttpResponse("Please proceed to room %d" %  (room_number))
+        else:
+            return HttpResponse("All doctors are busy right now, please relax in the waiting area. You are number %d in the queue." % (patient_queue.index(patient_id)+1))
 
 def delete(request, patient_id):
     try:
@@ -39,22 +40,36 @@ def delete(request, patient_id):
     except ValueError:
         raise Http404()
 
-    patient_queue.remove(patient_id)
-
-    return HttpResponse("You(id %d) have been removed from the queue. New queue is %s" % (patient_id, patient_queue))
+    if request.method == "DELETE":
+        patient_queue.remove(patient_id)
+        return HttpResponse("You(id %d) have been removed from the queue. New queue is %s" % (patient_id, patient_queue))
 
 
 def doctors(request):
-    return HttpResponse("There are currently %d doctors in the surgery. They are in rooms %s" % (len(doctors_busy),rooms_busy))
+    if request.method == "GET":
+        return HttpResponse("There are currently %d doctors available in the surgery. They are in rooms %s" % (len(doctors_avail), rooms_busy))
 
 def help_view(request):
-    return HttpResponse("Command list instruction")
+    if request.method == "GET":
+        return HttpResponse("Command list instruction")
 
 def login(request, doctor_id):
+    password = "password"
+    # ask the user for a password and check if the third argument is the correct password
+    # if it is, send an ok which will be checked atthe client side 
+
     try:
         doctor_id = int(doctor_id)
     except ValueError:
         raise Http404()
-    doctors_avail.append(doctor_id)
-    print(doctors_avail)
-    return HttpResponse("Doctor %d joined." % (doctor_id))
+
+    if request.method == "POST":
+        doctors_avail.append(doctor_id)
+        if patient_queue:
+            first_patient = patient_queue[0]
+            patient_queue.pop(0)
+            doctors_avail.remove(doctor_id)
+
+            return HttpResponse("DOCTOR %d joined and had taken PATIENT %d to a room." % (doctor_id, first_patient))
+        else:
+            return HttpResponse("There are no patients waiting!")
